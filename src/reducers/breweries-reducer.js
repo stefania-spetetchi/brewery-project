bbb018710a4d02da7d3456adb4f26cadcc425c1b
Stefania Spetetchi <steffi@balerion.lan>
2021-04-26 14:50:51 -0400

    updated state

--- src/reducers/breweries-reducer.js
+++ src/reducers/breweries-reducer.js
@@ -6,7 +6,15 @@ const breweryReducer = function (state = [], action) {
     case FETCH_BREWERY:
       return action.payload.map(function (b) {
         return {
-          name: b.name || ''
+          id: b.id || '',
+          name: b.name || '',
+          type: b.brewery_type || '',
+          address: b.street || '',
+          city: b.city || '',
+          state: b.state || '',
+          ZipCode: b.postal_code || '',
+          phone: b.phone || '',
+          website: b.website_url || '',
         }
       });
     default: return state;
